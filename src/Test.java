a447609665ed3cde39439e09a89dfb43a55c915f
purple57 <k1102391@students.katyisd.org>
2022-10-21 07:59:03 -0500

    yes?

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("aya this works?????");
         System.out.println("second try");
+        System.out.println("third try");
     }
 }
  //heheh
