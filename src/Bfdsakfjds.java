83963f0be9417600ad427e0cd7da94e921e22186
sirir123 <siri@ramineni.name>
2022-10-24 07:54:32 -0500

    rgrg

--- src/Bfdsakfjds.java
+++
@@ -1,5 +0,0 @@
-public class Bfdsakfjds {
-    public static void main(String[] args) {
-        System.out.println("i haget jsl");
-    }
-}
--- src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("aya this works?????");
-        System.out.println("second try");
-        System.out.println("third try");
-    }
-}
- //heheh
