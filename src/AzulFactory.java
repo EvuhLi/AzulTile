930c8a2a1cc24b16cc0797d50f0319af3ca8f7e4
EvuhLi <lievayifan@gmail.com>
2022-11-05 13:57:22 -0500

    fjasd

--- src/AzulFactory.java
+++ src/AzulFactory.java
@@ -9,7 +9,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 
-public class AzulFactory extends JPanel implements MouseListener {
+public class AzulFactory{
 	private BufferedImage factory, azulBoard;
 
 	Factory f1 = new Factory();
@@ -32,26 +32,25 @@ public class AzulFactory extends JPanel implements MouseListener {
 			System.out.println("Exception Error");
 			return;
 		}
-		addMouseListener(this);
 	}
 // method to paint the factory
 //put the 4 tiles there
 
 //another class to paint the 9 factories
-	public void paint(Graphics g) {
+	public void paint(Graphics g, int width, int height) {
 
-		g.drawImage(azulBoard, 0, 0, getWidth(), getHeight(), null);
-		int halfw = getWidth() / 2;
-		int halfh = getHeight() / 2;
+		g.drawImage(azulBoard, 0, 0, width, height, null);
+		int halfw = width / 2;
+		int halfh = height/ 2;
 //top left
-		f1.paintFactory(halfw / 4, halfh * 3 / 4, getWidth(), getHeight(), g);
+		f1.paintFactory(halfw / 4, halfh * 3 / 4, width, height, g);
 // //lower middle left
-		f2.paintFactory(halfw * 3 / 9, getHeight() * 14 / 17, getWidth(), getHeight(), g);
+		f2.paintFactory(halfw * 3 / 9, height * 14 / 17, width, height, g);
 
 // //left
-		f3.paintFactory(getWidth() / 15, getHeight() * 4 / 6, getWidth(), getHeight(), g);
+		f3.paintFactory(width / 15, height* 4 / 6, width, height, g);
 // //right m
-		f4.paintFactory(getWidth() * 2 / 6, getHeight() * 4 / 6, getWidth(), getHeight(), g);
+		f4.paintFactory(width * 2 / 6, height * 4 / 6, width, height, g);
 // //top left
 // f5.paintFactory(getWidth()/6,getHeight()/6, getWidth(), getHeight(), g);
 // //top right
@@ -65,25 +64,4 @@ public class AzulFactory extends JPanel implements MouseListener {
 
 	}
 
-	public void mousePressed(MouseEvent e) {
-	}
-
-	public void mouseReleased(MouseEvent e) {
-	}
-
-	public void mouseEntered(MouseEvent e) {
-	}
-
-	public void mouseExited(MouseEvent e) {
-	}
-
-	public void mouseClicked(MouseEvent e) {
-		int x = e.getX();
-		int y = e.getY();
-		System.out.println("loc is (" + x + "," + y + ")");
-		if (e.getButton() == e.BUTTON1) {
-
-		}
-		repaint();
-	}
 }
