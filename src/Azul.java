d1b734358cad7fe94caac1133a18796e9fd58751
EvuhLi <lievayifan@gmail.com>
2022-11-06 16:00:55 -0600

    fsdklja size pls work

--- src/Azul.java
+++ src/Azul.java
@@ -9,9 +9,10 @@ public class Azul extends JFrame {
 
 	public Azul(String title) {
 		super(title);
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		setResizable(false);
-		pack();
+		//setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//setResizable(false);
+		//pack();
+		setSize(1280, 720);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		add(new AzulPanel());
 		setVisible(true);
