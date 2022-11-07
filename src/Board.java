c67b7d88ce9fdf45ec3a8e6bab4e86f593c52c30
sirir123 <siri@ramineni.name>
2022-11-07 07:38:04 -0600

    ii

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,44 @@
 public class Board{
     Tile[][] b = new Tile[5][5];
-    int points = 0;
+    String[][] colors = new String[5][5];
+    int score = 0;
 
-    public Board(){}
+    public Board(){
+        colors[0][0] = "";
+        colors[0][1] = "";
+        colors[0][2] = "";
+        colors[0][3] = "";
+        colors[0][4] = "";
+        colors[1][0] = "";
+        colors[1][1] = "";
+        colors[1][2] = "";
+        colors[1][3] = "";
+        colors[1][4] = "";
+        colors[2][0] = "";
+        colors[2][1] = "";
+        colors[2][2] = "";
+        colors[2][3] = "";
+        colors[2][4] = "";
+        colors[3][0] = "";
+        colors[3][1] = "";
+        colors[3][2] = "";
+        colors[3][3] = "";
+        colors[3][4] = "";
+        colors[4][0] = "";
+        colors[4][1] = "";
+        colors[4][2] = "";
+        colors[4][3] = "";
+        colors[4][4] = "";
+    }
 
     public boolean colorInBoard(String color, int row){
         for(int c = 0; c < 5; c++){
-            if (b[row][c].getColor().equals(color)) return true;
+            if (colors[row][c].equals(color)) return true;
         }
         return false;
+    }
+
+    public void addTiles(){
+
     }
 }
