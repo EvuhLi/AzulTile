6646e4b8520391feaf6a88768d1958fbd78bc220
S8leeJ <jenna.snow.lee@gmail.com>
2022-11-09 17:11:11 -0600

    please

--- src/OneFactoryPanel.java
+++ src/OneFactoryPanel.java
@@ -7,14 +7,15 @@ import java.io.*;
 
 public class OneFactoryPanel {
 
-	private BufferedImage factory, tile, red, yellow;
+	private BufferedImage factory, tile, red, yellow, TChoose;
 
 	public OneFactoryPanel() {
 		try {
             tile = ImageIO.read(new File("src/images/blue tile.png"));
 			red = ImageIO.read(new File("src/images/red tile.png"));
 			yellow = ImageIO.read(new File("src/images/yellow tile.png"));
-          
+			TChoose = ImageIO.read(new File("src/images/TileChoosing.png"));
+
             factory = ImageIO.read(new File("src/images/factory.png"));
 		} catch (Exception e) {
 			return;
@@ -29,7 +30,7 @@ public class OneFactoryPanel {
 		//private static final int HEIGHT = 1080;
 		g.drawImage(factory, w, h, size, size, null);
 
-		g.drawImage(factory, w, h, size, size, null);
+		g.drawImage(TChoose, 700, -50, 600, 400, null);
 		g.drawImage(tile, w + ww/30, h + hh/11, tSize, tSize, null);
 		g.drawImage(red, w + ww/30, h + hh/18, tSize, tSize, null);
 		g.drawImage(tile, w + ww/20, h + hh/18, tSize, tSize, null);
