01645ede48c60182dd05a58c6ff711c735a25e8b
S8leeJ <jenna.snow.lee@gmail.com>
2022-11-12 20:51:22 -0600

    d

--- src/OneFactoryPanel.java
+++ src/OneFactoryPanel.java
@@ -5,43 +5,98 @@ import javax.swing.*;
 import java.io.*;
 import java.util.*;
 
+
 public class OneFactoryPanel {
 
-	private ArrayList<Tile> tiles;
-	private BufferedImage factory, tile, red, yellow, TChoose;
+    private BufferedImage factory, blue, red, yellow, TChoose, teal, black;
+    private ArrayList<BufferedImage>TArr;
+    private TreeMap<String, Integer>possibleT;
 
-	public OneFactoryPanel() {
-		try {
-			tiles = new ArrayList<>();
-            tile = ImageIO.read(new File("src/images/blue tile.png"));
-			red = ImageIO.read(new File("src/images/red tile.png"));
-			yellow = ImageIO.read(new File("src/images/yellow tile.png"));
-			TChoose = ImageIO.read(new File("src/images/TileChoosing.png"));
+	private boolean M;
+	private Factory ffs;
+    public OneFactoryPanel() {
+        try {
+
+			ffs = new Factory(M);
+            TArr = new ArrayList<>();
+            possibleT = new TreeMap<>();
+            blue = ImageIO.read(new File("src/images/blue tile.png"));
+            red = ImageIO.read(new File("src/images/red tile.png"));
+            yellow = ImageIO.read(new File("src/images/yellow tile.png"));
+            teal = ImageIO.read(new File("src/images/teal tile.png"));
+            black = ImageIO.read(new File("src/images/black tile.png"));
+            TChoose = ImageIO.read(new File("src/images/TileChoosing.png"));
             factory = ImageIO.read(new File("src/images/factory.png"));
-		} catch (Exception e) {
-			return;
-		}
+            addTiles();
+           
+        } catch (Exception e) {
+            return;
+        }
+    }
 
+	private void setisMiddle(boolean M){
+	M=M;
 	}
-	public void getArray(ArrayList<Tile>a){
-		tiles = a;
-	}
+    public void setArray(ArrayList<BufferedImage>arr){
+        TArr = arr;
+    }
+    public void paintFactory(int w, int h, int ww, int hh, Graphics g) {
+        int size = ww / 10;
+        int tSize = ww / 50;
+        g.drawImage(factory, w, h, size, size, null);
+        int fW = w + ww/30;
+        int fH = h+hh/11;
+        for(int i = 0; i<TArr.size(); i++){
+            if(i==0)
+                g.drawImage(TArr.get(i), fW, fH, tSize, tSize, null);
+                else if(i==1)
+                g.drawImage(TArr.get(i), w + ww/20, h + hh/18, tSize, tSize, null);
+                else if(i==2)
+                g.drawImage(TArr.get(i), w + ww/30, h + hh/18, tSize, tSize, null);
+                else
+                g.drawImage(TArr.get(i), w + ww/20, h + hh/11, tSize, tSize, null);
+        }
+    }
+public void addTiles(){
+    if(TArr.size()<=4){
+        TArr.add(red);
+        TArr.add(yellow);
+        TArr.add(red);
+        TArr.add(yellow);
+    }
+}
+public int getSize(){
+    return TArr.size();
+}
+    public void paintChoosing(Graphics g){
+        g.drawImage(TChoose, 600, -55, 700, 400, null);
+        HashMap<BufferedImage, Integer>ava = new HashMap<>();
+        for(int i = 0; i<TArr.size(); i++){
+            BufferedImage temp = TArr.get(i);
+            if(!ava.containsKey(temp)){
+                ava.put(temp, 1);
+            }
+            else{
+                ava.put(temp, ava.get(temp)+1);
+            } 
+        }
+	
+        Set<BufferedImage> s = ava.keySet();
+        Iterator<BufferedImage> iter = s.iterator();
+		
+        while(iter.hasNext()){
+            BufferedImage b = iter.next();
+			int x = ava.get(b);
+			g.setFont(new Font("Times New Roman", Font.PLAIN, 35));
 
-	public void paintChoosing(Graphics g){
-		g.drawImage(TChoose, 600, -50, 700, 400, null);
+			if(b.equals(yellow)){
+				g.drawString(x+"", 1029, 68);
+			}
+			else if(b.equals(red)){
+				g.drawString(x+"", 1029, 142);
+			}
 
-	}
-
-	public void paintFactory(int w, int h, int ww, int hh, Graphics g) {
-		int size = ww / 10;
-		int tSize = ww / 50;
-		//private static final int WIDTH = 1920;
-		//private static final int HEIGHT = 1080;
-		g.drawImage(factory, w, h, size, size, null);
-		g.drawImage(tile, w + ww/30, h + hh/11, tSize, tSize, null);
-		g.drawImage(red, w + ww/30, h + hh/18, tSize, tSize, null);
-		g.drawImage(tile, w + ww/20, h + hh/18, tSize, tSize, null);
-		g.drawImage(yellow, w + ww/20, h + hh/11, tSize, tSize, null);
-	}
+        }
+    }
 
 }
